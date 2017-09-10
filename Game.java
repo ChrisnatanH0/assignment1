8ce2d7ba6e8f4e150180fae0bffad9a9f7c60294
Chrisnatan HO <chrisnatanhadi.oetomo@my.jcu.edu.au>
2017-09-10 22:39:36 +0800

    - Added lines to check if a player chose to pass or lost all the cards - Added some end game lines - Some minor fixes

--- Game.java
+++ Game.java
@@ -45,6 +45,7 @@ public class Game {
         if (numOfPlayers >= 3) ShuffleCards(cards, player3Cards);
         if (numOfPlayers == 4) ShuffleCards(cards,player4Cards);
 
+        //The game runs
         while (flag) {
             //Display list of cards
             switch (playerTurn) {
@@ -70,11 +71,19 @@ public class Game {
             cardSelect = playerInput.nextInt();
             //Error checking
             do {
-                if (cardSelect < 0 || cardSelect >= currentCardSet.size() || cardSelect != 999) {
-                    System.out.println("ERROR! Invalid input!");
-                    System.out.print("Select your card: ");
-                    cardSelect = playerInput.nextInt();
-                } else if (firstTurn && superTrumps.contains(currentCardSet.get(cardSelect).getName())){
+                if (cardSelect < 0 || cardSelect >= currentCardSet.size()) {
+                    //If the player chooses to pass
+                    if (cardSelect == 999) {
+                        playerPass.add(playerTurn);
+                        break;
+                    } else {
+                        System.out.println("ERROR! Invalid input!");
+                        System.out.print("Select your card: ");
+                        cardSelect = playerInput.nextInt();
+                    }
+                }
+                //Players cannot use their SuperTrump cards in the first turn
+                else if (firstTurn && superTrumps.contains(currentCardSet.get(cardSelect).getName())){
                     System.out.println("You cannot use a SuperTrump card right now!");
                     System.out.print("Select your card: ");
                     cardSelect = playerInput.nextInt();
@@ -97,14 +106,15 @@ public class Game {
                 //If it is a regular card
                 if (!(superTrumps.contains(currentCardSet.get(cardSelect).getName()))) {
                     valueSelect = currentCardSet.get(cardSelect).getValueBasedOnInput(trumpSelect);
-                    if (valueSelect < currentHighest) {
+                    while (valueSelect <= currentHighest) {
                         System.out.println("The value is not high enough!");
                         System.out.println(currentHighestString);
-                    } else {
-                        currentHighest = valueSelect;
-                        currentHighestString = "The current highest " + trumpSelect + ": " + currentCardSet.get(cardSelect).getStringBasedOnInput(trumpSelect);
-                        System.out.println(currentHighestString);
+                        DisplayCards(currentCardSet);
                     }
+                    currentHighest = valueSelect;
+                    currentHighestString = "The current highest " + trumpSelect + ": " + currentCardSet.get(cardSelect).getStringBasedOnInput(trumpSelect);
+                    System.out.println(currentHighestString);
+
                 }
 
                 //If it is a SuperTrump card
@@ -121,7 +131,41 @@ public class Game {
                     System.out.println(currentHighestString);
                 }
             }
+
+            //Check if the player loses all of the cards
+            if (currentCardSet.size() == 0) {
+                System.out.println("Player " + playerTurn + " has no more cards!");
+                playerWins.add(playerTurn);
+            }
+
+            //Change the current player's turn
+            do {
+                playerTurn++;
+                if (playerPass.contains(playerTurn)) playerTurn++;
+                else if (playerWins.contains(playerTurn)) playerTurn++;
+                else if (playerTurn > 4) playerTurn = 1;
+                else break;
+            } while(true);
+
+            //Check if all but one player chose to pass
+            if (playerPass.size() == numOfPlayers - 1) {
+                playerPass.clear();
+                firstTurn = true;
+            }
+
+            //Check if all but one player wins
+            if (playerWins.size() == numOfPlayers - 1) {
+                flag = false;
+            }
         }
+
+        //Prints out the list of winners
+        System.out.println("GAME OVER!");
+        System.out.println("Winning Players:");
+        for (int i = 0; i < playerWins.size(); i++) {
+            System.out.println(i+1 + ". Player " + playerWins.get(i));
+        }
+        System.out.println("Thank you for playing!");
     }
 
     //Select a trump
